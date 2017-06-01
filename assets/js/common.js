208b895e0e567a5887d84334327c3951584fd6dc
joraSir <siriye@mail.ru>
2017-06-01 15:15:14 +0300

    +table styles

--- assets/js/common.js
+++ assets/js/common.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
     $(window).load(function() {
         $(".loader").delay(400).fadeOut("slow");
     });
-
+    $(".post_content table").wrap("<div class='post_table_w'></div>");
     svg4everybody({});
 
     $(".menu-button").click(function(e) {
